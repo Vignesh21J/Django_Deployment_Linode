58dfb4813674184b37ec9df9acb11183992db6d6
Vignesh21J <vignesh11212003@gmail.com>
2026-02-04 11:15:50 +0530

    feat: email sending via Brevo

--- backend-drf/clickmart_main/settings.py
+++ backend-drf/clickmart_main/settings.py
@@ -161,12 +161,15 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST="smtp.gmail.com"
-EMAIL_PORT=587
-EMAIL_USE_TLS=True
+EMAIL_HOST = 'smtp-relay.brevo.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = 'ClickMart <orders@clickmartmart.shop>'
+
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
